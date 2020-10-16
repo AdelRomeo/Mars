773fefc06dc00732a2b9ae0d82b88d49eb8c2397
AdelRomeo <11cage11@mail.ru>
2020-10-17 01:10:11 +0700

    full scss main-header

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 const watch = require('gulp-watch');
 
 gulp.task('sass-compile', () =>{
     return gulp.src('./src/styles/main-style.scss') // путь откуда будут браться файлы
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(concat('main.css')) //сборка нескольких файлов в один
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./build/styles/')) //путь куда ляжет конечный файл
