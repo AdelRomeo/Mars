e471e813d2f07b27a448bfb3cb8cba43ce098d27
AdelRomeo <11cage11@mail.ru>
2020-10-23 20:19:51 +0700

    better visual js code v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,17 @@
 const selectBtns = document.querySelectorAll('.survival-title>button'); //кнопки
-const selectMain = document.querySelector('.survival-title'); //родительский элемент кнопок
 const selectArticles = document.querySelectorAll('.survival-item'); //абзацы
 
-//вешаем обработчик на родителя всех кнопок
-selectMain.addEventListener('click', (e)=>{
-    //перебераем все кнопки
-    for (let i=0; i < selectBtns.length; i++){
-        // если нажата любая из кнопок
-        if (e.target === selectBtns[i]){
 
-            //делаем полупрозрачными все кнопки
-            selectBtns[i].style.opacity = '0.5';
-            //делаем прозрачными все абзацы
-            selectArticles[i].style.opacity = '0';
 
-            //делаем кнопку по которой кликнули полностью видимой
-            e.target.style.opacity = '1';
-            //делаем абзац идекс которого соответствует кнопке по которой кликнули видимым
-            setTimeout(()=>selectArticles[i].style.opacity = '1', 600);
+for (let i=0; i < selectBtns.length; i++){
+    selectBtns[i].addEventListener('click', (e)=>{
+        for (let j = 0; j < selectArticles.length; j++){
+            selectBtns[j].style.opacity = '0.5'; //делаем полупрозрачными все кнопки
+            selectArticles[j].style.opacity = '0'; //делаем прозрачными все абзацы
         }
-    }
-});
+        e.target.style.opacity = '1'; //делаем кнопку по которой кликнули полностью видимой
+        setTimeout(()=>{
+            selectArticles[i].style.opacity = '1'; //делаем абзац порядковый номер которого равен кнопке по которой кликнули видимым
+        }, 600);
+    })
+}
