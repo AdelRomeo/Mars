69891eb009f20ecfce43788abec4f269fef488d3
AdelRomeo <11cage11@mail.ru>
2020-10-23 23:29:33 +0700

    better js visual final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,32 @@
 const selectBtns = document.querySelectorAll('.survival-title>button'); //кнопки
-const selectMain = document.querySelector('.survival-title');
+const selectMain = document.querySelector('.survival-title'); //родительский элемент кнопок
 const selectArticles = document.querySelectorAll('.survival-item'); //абзацы
 
-
-
-// for (let i=0; i < selectBtns.length; i++){
-//     selectBtns[i].addEventListener('click', (e)=>{
-//         for (let j = 0; j < selectArticles.length; j++){
-//             selectBtns[j].style.opacity = '0.5'; //делаем полупрозрачными все кнопки
-//             selectArticles[j].style.opacity = '0'; //делаем прозрачными все абзацы
-//         }
-//         e.target.style.opacity = '1'; //делаем кнопку по которой кликнули полностью видимой
-//         setTimeout(()=>{
-//             selectArticles[i].style.opacity = '1'; //делаем абзац порядковый номер которого равен кнопке по которой кликнули видимым
-//         }, 600);
-//     })
-// }
-
+//вешаем обработчик на родителя всех кнопок
 selectMain.addEventListener('click', (event)=>{
+
+    //проверяем на что нажали
     if (event.target.tagName === 'BUTTON'){
 
-        //обнуление стилий для всех
-        for (let j = 0; j < selectArticles.length; j++){
-            selectBtns[j].style.opacity = '0.5'; //делаем полупрозрачными все кнопки
-            selectArticles[j].style.opacity = '0'; //делаем прозрачными все абзацы
+        //перебираем все кнопки
+        for (let i = 0; i < selectBtns.length; i++){
+
+            //добавляем кнопкам data атрибут
+            selectBtns[i].dataset.index = i;
+
+            //делаем полупрозрачными все кнопки
+            selectBtns[i].style.opacity = '0.5';
+            //делаем прозрачными все абзацы
+            selectArticles[i].style.opacity = '0';
         }
 
+        //делаем кнопку на которую нажали более яркой
         event.target.style.opacity = '1';
-        console.log(event.target.dataset.index);
-        setTimeout(()=>{
-             selectArticles[event.target.dataset.index].style.opacity = '1'; //делаем абзац порядковый номер которого равен кнопке по которой кликнули видимым
-         }, 600);
+
+        //берем данные из data атрибута кнопки на которую нажали.
+        let index = event.target.dataset.index;
+
+        //делаем абзац порядковый номер которого равен кнопке по покоторой кликнули видимым
+        setTimeout(()=> selectArticles[index].style.opacity = '1', 600);
     }
 });
